93c5a4268470842824de344d9fba1801b3985c56
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-04-13 18:43:36 +0200

    fix: simplify AppLayout by replacing custom layout with AppShell component

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,13 +1,5 @@
-import React from 'react';
-import Sidebar from '../../components/Sidebar';
+import AppShell from '../../components/AppShell';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex bg-slate-50 dark:bg-slate-900 text-black dark:text-white">
-      <aside className="w-72 p-6 border-r border-slate-200 dark:border-slate-800">
-        <Sidebar />
-      </aside>
-      <main className="flex-1 p-6">{children}</main>
-    </div>
-  );
+  return <AppShell>{children}</AppShell>;
 }
